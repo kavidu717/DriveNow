d6cff6d488b2246a026a9758a1f38fea2019459e
kavidu717 <dushmanthakavidu143@gmail.com>
2026-06-05 15:42:26 +0530

    feat: add login functionality with JWT authentication

--- backend/src/controller/authController.js
+++ backend/src/controller/authController.js
@@ -89,3 +89,61 @@ import jsonwebtoken from 'jsonwebtoken';
     });
   }
 };
+
+export const loginUser=async(req,res)=>{
+    try{
+        const {email,password}=req.body
+
+           const user = await User.findOne({ email }).select("+password");
+   
+
+    if (!user) {
+      return res.status(400).
+      json({
+         message: "Invalid credentials" 
+        });
+
+    }
+       if (!user.isVerified) {
+
+      return res.status(400)
+      .json({
+        message: "Please verify your email first"
+      });
+    }
+
+     const match = await bcrypt.compare(password, user.password);
+
+    if (!match) {
+      return res.status(400)
+      .json({
+         message: "Invalid credentials" 
+        });
+    }
+     const token = jsonwebtoken.sign(
+      { id: user._id, role: user.role },
+      process.env.JWT_SECRET,
+      { expiresIn: "1d" }
+    );
+
+    res.status(200).json({
+      message: "Login successful",
+      token
+    });
+
+
+
+
+
+  
+
+
+
+    }catch(error){
+     res.status(500)
+     .json({
+        message:error.message
+     })
+    }
+
+}
--- backend/src/routes/userRoutes.js
+++ backend/src/routes/userRoutes.js
@@ -1,10 +1,12 @@
 import express from "express";
-import { registerUser,verifyOTP } from "../controller/authController.js";
+import { loginUser, registerUser,verifyOTP } from "../controller/authController.js";
 
 const router = express.Router();
 
 router.post("/register", registerUser);
 router.post("/verify-otp",verifyOTP);
+router.post("/login",loginUser);
+
 
 
 export default router;
