b93141dacb05a87ce86f164e2e00e73e3ec268f1
kavidu717 <dushmanthakavidu143@gmail.com>
2026-06-06 19:49:37 +0530

    feat: add vehicle model schema

---
+++ backend/src/models/vehicleModel.js
@@ -0,0 +1,66 @@
+import mongoose from "mongoose";
+
+const vehicleSchema = new mongoose.Schema({
+       name:{
+              type: String,
+              required: true,
+              trim: true
+       },
+       type: {
+              type: String,
+              required: true
+       },
+       brand:{
+              type: String,
+              required: true
+       },
+       model: {
+              type: String,
+              required: true
+       },
+       description: {
+              type: String,
+              required: true
+       },
+       pricePerKm:{
+              type: Number,
+              required: true
+       },
+       image: {
+              type: String,
+              required: true
+       },
+       availability:{
+              type: Boolean,
+              default: true
+       },
+       isBlocked: {
+              type: Boolean,
+              default: false
+       },
+       rating:{
+              type: Number,
+              default: 0
+       },
+       numReviews: {
+              type: Number,
+              default: 0
+       },
+       defaultKm:{
+              type: Number,
+              default: 50
+       },
+       createdBy: {
+              type: mongoose.Schema.Types.ObjectId,
+              ref: "User",
+              required: true
+       }
+              
+
+},{timestamps:true})
+
+ const Vehicle = mongoose.model("Vehicle", vehicleSchema);
+
+
+ export default Vehicle
+    
