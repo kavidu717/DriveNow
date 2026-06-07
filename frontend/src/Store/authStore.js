032adddcf4b6891024de8372da62ed435ebdaf3b
kavidu717 <dushmanthakavidu143@gmail.com>
2026-06-08 00:26:39 +0530

    feat:update login register and log out authstore.js

--- frontend/src/Store/authStore.js
+++ frontend/src/Store/authStore.js
@@ -1,37 +1,99 @@
 import { create } from "zustand";
+import API from "../api/axios.js";
 
 const useAuthStore = create((set) => ({
+
   user: JSON.parse(localStorage.getItem("user")) || null,
   token: localStorage.getItem("token") || null,
   isAuthenticated: !!localStorage.getItem("token"),
 
-  login: (user, token) => {
-    localStorage.setItem("token", token);
-    localStorage.setItem("user", JSON.stringify(user));
 
-    set({
-      user,
-      token,
-      isAuthenticated: true,
-    });
+  register: async (firstName, lastName, email, password) => {
+    try {
+      const { data } = await API.post(
+        "/auth/register",
+        {
+          firstName,
+          lastName,
+          email,
+          password
+        }
+      );
+
+      return data;
+    } catch (error) {
+      throw (
+        error.response?.data?.message ||
+        "Registration failed"
+      );
+    }
   },
 
+
+  verifyOtp: async (email, otp) => {
+    try {
+      const { data } = await API.post(
+        "/auth/verify-otp",
+        {
+          email,
+          otp,
+        }
+      );
+
+      return data;
+    } catch (error) {
+      throw (
+        error.response?.data?.message ||
+        "OTP verification failed"
+      );
+    }
+  },
+
+
+  login: async (email, password) => {
+    try {
+      const { data } = await API.post(
+        "/auth/login",
+        {
+          email,
+          password,
+        }
+      );
+
+      const { user, token } = data;
+
+      localStorage.setItem(
+        "user",
+        JSON.stringify(user)
+      );
+      localStorage.setItem("token", token);
+
+      set({
+        user,
+        token,
+        isAuthenticated: true,
+      });
+
+      return data;
+    } catch (error) {
+      throw (
+        error.response?.data?.message ||
+        "Login failed"
+      );
+    }
+  },
+
+  // ======================
+  // LOGOUT
+  // ======================
   logout: () => {
-    localStorage.removeItem("token");
     localStorage.removeItem("user");
+    localStorage.removeItem("token");
 
     set({
       user: null,
       token: null,
       isAuthenticated: false,
-    });
-  },
-
-  setUser: (user) => {
-    localStorage.setItem("user", JSON.stringify(user));
-
-    set({
-      user,
     });
   },
 }));
