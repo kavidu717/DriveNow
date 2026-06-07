a588eedc2e149712984c3b6304be2b0e39815056
kavidu717 <dushmanthakavidu143@gmail.com>
2026-06-07 23:11:27 +0530

    chore: configure axios instance with API base URL and auth interceptor

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,17 @@
 import axios from "axios";
 
-const API=axios.create({
-    baseURL:"http://localhost:3000/api/v1"
-})
+const API = axios.create({
+  baseURL: "http://localhost:3000/api/v1",
+});
 
-export default API
+API.interceptors.request.use((config) => {
+  const token = localStorage.getItem("token");
+
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+
+  return config;
+});
+
+export default API;
