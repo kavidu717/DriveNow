64e47060fc17b10a4c2627d8b1d57b0e2bb09f1f
kavidu717 <dushmanthakavidu143@gmail.com>
2026-06-08 00:53:44 +0530

    feat:import react tost for the main file

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,11 +3,23 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
+import { Toaster } from 'react-hot-toast'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
+
       <App />
+      <Toaster 
+      position="top-right"
+      toastOptions={{
+        duration: 3000,
+        style: {
+          background: '#333',
+          color: '#fff',
+        },
+      }}
+    />
     </BrowserRouter>
    
   </StrictMode>,
