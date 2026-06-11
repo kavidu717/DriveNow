8eb067e027eca77afc9f3246b12560fd7955c110
kavidu717 <dushmanthakavidu143@gmail.com>
2026-06-12 01:10:12 +0530

    feat:update backend url

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,19 +1,18 @@
 import axios from "axios";
-import useAuthStore from "../Store/authStore.js"; // ⚠️ ඔයාගේ authStore එක තියෙන නිවැරදි path එක දෙන්න
+import useAuthStore from "../Store/authStore.js";
 
 const API = axios.create({
-  baseURL: "http://localhost:3000/api/v1", // ඔයාගේ Backend base URL එක
+  baseURL: "https://drivenow-tbgt.onrender.com/api/v1", 
 });
 
-// ✅ AXIOS INTERCEPTOR
-// Backend එකට ඕනෑම Request එකක් යන්න කලින් මේ කොටස ක්‍රියාත්මක වෙනවා
+
 API.interceptors.request.use(
   (config) => {
-    // ඔයාගේ Zustand Auth Store එකෙන් කෙලින්ම වත්මන් token එක ලබා ගන්නවා
+   
     const token = useAuthStore.getState().token;
 
     if (token) {
-      // සාර්ථකව token එක තිබේ නම් එය Authorization Header එකට auto එකතු කරනවා
+      
       config.headers.Authorization = `Bearer ${token}`;
     }
     
