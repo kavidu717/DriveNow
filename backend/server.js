61b6a311ca98f82fe1ee6f6fa97ddab0aff9107b
kavidu717 <dushmanthakavidu143@gmail.com>
2026-06-12 01:06:33 +0530

    feat:add environment configuration using dotenv

--- backend/server.js
+++ backend/server.js
@@ -15,6 +15,7 @@ const app = express();
 app.use(cors());
 
 connectDB();
+const PORT=process.env.PORT||3000
 
 app.get('/', (req, res) => {
     res.send('drive now backend is running');
@@ -36,6 +37,6 @@ app.use("/api/v1/payment",paymentRouter);
 
 
 
- app.listen(3000, () => {
+ app.listen(PORT, () => {
     console.log('Server is running on port 3000');
  })
