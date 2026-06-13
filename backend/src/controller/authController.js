941211a389e2623884b5c56bc5b8aa1e6274f4ab
kavidu717 <dushmanthakavidu143@gmail.com>
2026-06-13 23:32:23 +0530

    fix:add the console logo to show hosting error in register

--- backend/src/controller/authController.js
+++ backend/src/controller/authController.js
@@ -44,6 +44,7 @@ import jsonwebtoken from 'jsonwebtoken';
 
 
     }catch(error){
+      console.log(error);
             res.status(500)
             .json({
                  message:error.message 
