46dbd9cbccc845243f4c9b0e6528e19a08baae50
kavidu717 <dushmanthakavidu143@gmail.com>
2026-06-14 00:01:36 +0530

    fix: change nodemailer port to 587 to resolve render connection timeout

--- backend/src/utils/sendEmail.js
+++ backend/src/utils/sendEmail.js
@@ -3,13 +3,13 @@ import nodemailer from 'nodemailer';
 const sendEmail = async (to, subject, text) => {
   const transporter = nodemailer.createTransport({
     host: 'smtp.gmail.com',
-    port: 465,
-    secure: true, // true for 465 port
+    port: 587,         
+    secure: false,      
+    requireTLS: true, 
     auth: {
       user: process.env.EMAIL_USER,
       pass: process.env.EMAIL_PASS
     },
-  
     tls: {
       rejectUnauthorized: false
     }
